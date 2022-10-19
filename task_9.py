ffbd8c9db11776a16fc3fc7bc3aca355c794b9da
annshmaka <115432602+annshmaka@users.noreply.github.com>
2022-10-19 23:23:08 +0300

    Лабораторная работа 3
    Исправлено 6-ое задание, решено 8-ое

---
+++ task_9.py
@@ -0,0 +1,13 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+# increase = 0.03  # рост цен
+increase = 1.03  # для удобства
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+
+# TODO Оформить решение
+
+for need_money in range(1, months + 1):
+   need_money = spend - salary
+   spend = spend * 1.03
+print(round(need_money))
